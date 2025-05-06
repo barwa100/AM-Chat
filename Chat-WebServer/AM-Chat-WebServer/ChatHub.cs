633c82c6f3399a64048b969c48f0bfebf557329a
Bartosz Barwiński <barwinski.b@protonmail.com>
2025-05-06 23:58:35 +0200

    Added backend files

---
+++ Chat-WebServer/AM-Chat-WebServer/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace AM_Chat_WebServer;
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string msg)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", msg);
+    }
+    
+}
---
+++ Chat-WebServer/AM-Chat-WebServer/Data/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace AM_Chat_WebServer.Data.Models;
+
+public class User : IIdentifiable
+{
+    public ulong Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public List<Channel> Channels { get; set; }
+    public List<Message> Messages { get; set; }
+}
