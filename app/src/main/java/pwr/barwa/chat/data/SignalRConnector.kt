448faba2750958792db2d9d38845d04e6d2134fa
Bartosz Barwiński <barwinski.b@protonmail.com>
2025-05-12 16:31:41 +0200

    Debug ws connection

---
+++ app/src/main/java/pwr/barwa/chat/data/SignalRConnector.kt
@@ -0,0 +1,45 @@
+package pwr.barwa.chat.data
+
+import android.content.Context
+import androidx.room.Room
+import com.microsoft.signalr.HubConnection
+import com.microsoft.signalr.HubConnectionBuilder
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class SignalRConnector {
+
+    private val hubConnection: HubConnection by lazy {
+        HubConnectionBuilder
+            .create("http://10.0.2.2:5093/ws")
+            .build()
+    }
+
+    private val _messages = MutableStateFlow<List<String>>(emptyList())
+    val messages: StateFlow<List<String>> = _messages
+
+
+    suspend fun startConnection() {
+        hubConnection.on("ReceiveMessage", { message ->
+            _messages.value += message
+        }, String::class.java)
+        hubConnection.start().blockingAwait()
+    }
+    fun stopConnection() {
+        hubConnection.stop()
+    }
+    fun sendMessage(message: String) {
+        hubConnection.send("SendMessage", message)
+    }
+    companion object {
+
+        @Volatile
+        private var Instance : SignalRConnector? = null
+        fun getInstance(context: Context): SignalRConnector {
+            return Instance ?: synchronized(this) {
+                SignalRConnector()
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/pwr/barwa/chat/ui/ChatViewModel.kt
@@ -0,0 +1,34 @@
+package pwr.barwa.chat.ui
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import pwr.barwa.chat.data.SignalRConnector
+
+class ChatViewModel(private val signalRConnector: SignalRConnector) : ViewModel() {
+
+    private val _messages = mutableStateOf<List<String>>(emptyList())
+    val messages: State<List<String>> = _messages
+
+    init {
+        viewModelScope.launch {
+            signalRConnector.messages.collect { newMessages ->
+                _messages.value = newMessages
+            }
+        }
+    }
+
+    suspend fun connectToChat() {
+        signalRConnector.startConnection()
+    }
+
+    fun disconnectFromChat() {
+        signalRConnector.stopConnection()
+    }
+
+    fun sendMessage(message: String) {
+        signalRConnector.sendMessage(message)
+    }
+}
